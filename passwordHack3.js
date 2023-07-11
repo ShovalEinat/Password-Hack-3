5bc678dcbbf89ab2064fd01b46a3bb0cb97d31b0
ShovalEinat <125089605+ShovalEinat@users.noreply.github.com>
2023-07-12 00:43:21 +0300

    Add files via upload

---
+++ passwordHack3.js
@@ -0,0 +1,18 @@
+let NBp0v17rc9P7 = "QsEpD1pZKc3q";
+let QsEpDIpZKc3q = "NBp0v17rc9P7";
+let NBp0vI7rc9P7 = "k2txeBvdPUXS";
+
+function checkPass() {
+    x = document.getElementById('userPassInput').value;
+    if (x == QsEpDIpZKc3q + "zN1x" + NBp0v17rc9P7) {
+        alert("Nice you got the right password!");
+        window.location = "https://shovaleinat.github.io/";
+       }
+       else if(document.getElementById("userPassInput").value == ""){
+        alert("You need to type a password!");
+       }
+       else{
+        alert("The password is incorrect, try again.");
+        document.getElementById("userPassInput").value = "";
+       };
+    };
